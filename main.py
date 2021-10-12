48391dfb01f692abac0dacfebf1d2338fcf22779
William Besnard <williambesnard1@gmail.com>
2021-10-12 15:28:01 +0200

    CSVBuilder + better OO

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from models.heatmap import HeatMapBuilder
+from models.csv_builder import CsvBuilder
 
-builder = HeatMapBuilder()
+#builder = HeatMapBuilder()
 
-heatmap = builder.generate_heatmap(group="Patients", file_name="P_0011_73_M_JT.xlsx", sheet_name="mosob01.jpg - G")
+#heatmap = builder.generate_heatmap(group="Patients", file_name="P_0011_73_M_JT.xlsx", sheet_name="mosob01.jpg - G")
 
-builder.print_heatmap(heatmap)
+#builder.print_heatmap(heatmap)
+csv_builder = CsvBuilder()
+
+files_names = csv_builder.find_all_datas(group="Patients")
+print(files_names)
+
+#csv_builder.generate_csv()
--- models/heatmap.py
+++ models/heatmap.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class HeatMapBuilder:
-    def __init__(self, main_path="Acquisitions_Eye_tracking_objets_visages_Fix_Seq1", heatmap_shape=(64, 64), images_shape=(1000, 1000)):
+    def __init__(self, main_path="Acquisitions_Eye_tracking_objets_visages_Fix_Seq1",
+                 heatmap_shape=(64, 64), images_shape=(1000, 1000)):
         self.main_path = main_path
         self.heatmap_shape = heatmap_shape
         self.images_shape = images_shape
@@ -14,22 +15,37 @@ class HeatMapBuilder:
         return data
 
     def generate_heatmap(self, group, file_name, sheet_name):
+        """
+
+        :param group:
+        :param file_name:
+        :param sheet_name:
+        :return: HeatMap
+        """
         data = self.read_file(group, file_name, sheet_name)
 
-        heatmap = np.zeros(self.heatmap_shape)
+        heatmap = HeatMap(core=np.zeros(self.heatmap_shape))
 
         for index, row in data.iterrows():
             print(row['x'], row['y'])
             h_x = round(self.heatmap_shape[0] * (row['x'] / self.images_shape[0]))
             h_y = round(self.heatmap_shape[1] * (row['y'] / self.images_shape[1]))
             print(h_x, h_y)
-            heatmap[h_x][h_y] += 1
+            heatmap.core[h_x][h_y] += 1
 
         return heatmap
+
+    def generate_all(self):
+        pass
+
+class HeatMap:
+
+    def __init__(self, core):
+        self.core = core
 
     def print_heatmap(self, heatmap):
         plt.imshow(heatmap, cmap='hot', interpolation='nearest')
         plt.show()
 
-    def generate_all(self):
+    def save_to_(self):
         pass
