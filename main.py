bc8036d5eae37640491ffdbf5d611de9b801bff8
Rémi B <7360391+remibrnl@users.noreply.github.com>
2021-11-23 15:42:44 +0100

    Autoencoder ?

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
 
     #print(labels)
 
-    learning_cnn(images, labels)
+    #learning_cnn(images, labels)
+    learning_autoencoder(images, labels)
--- models/deep_nn.py
+++ models/deep_nn.py
@@ -70,29 +70,6 @@ def fully_connectec_model():
     return model
 
 
-def cnn_model():
-    model = tf.keras.Sequential([
-        # Partie convultionnelle
-        tf.keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(64, 64, 1)),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
-
-        # Classification
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(2)
-    ])
-
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    model.summary()
-
-    return model
-
-
 def learning(images, labels):
     # data gathering
     images = np.asarray(images)
@@ -124,10 +101,68 @@ def learning_cnn(images, labels):
     test_images = np.array(test_images)
     test_images = test_images.reshape((test_images.shape[0], 64, 64, 1))
     # create ml model
-    model = cnn_model()
+    model = tf.keras.Sequential([
+        # Partie convultionnelle
+        tf.keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(64, 64, 1)),
+        # tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+        # tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+
+        # Classification
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(2)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    model.summary()
 
     model.fit(train_images, train_labels, epochs=10)
 
     test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
     print("Test accuracy: ", test_acc)
+
+
+def learning_autoencoder(images, labels):
+    # data gathering
+    images = np.asarray(images)
+    labels = np.asarray(labels)
+    # split into train & test datasets
+    (train_images, train_labels), (test_images, test_labels) = split_datas(0.8, images, labels)
+
+    train_images = np.array(train_images)
+    train_images = train_images.reshape((train_images.shape[0], 64, 64, 1))
+    train_images = train_images.astype("float32")
+
+    test_images = np.array(test_images)
+    test_images = test_images.reshape((test_images.shape[0], 64, 64, 1))
+
+    print(train_images.shape)
+    print(test_images.shape)
+
+    input_img = tf.keras.Input(shape=(64, 64, 1))
+    x = tf.keras.layers.Conv2D(32, (3, 3), activation="relu", padding="same")(input_img)
+    x = tf.keras.layers.MaxPooling2D((2, 2), padding="same")(x)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation="relu", padding="same")(x)
+    x = tf.keras.layers.MaxPooling2D((2, 2), padding="same")(x)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation="relu", padding="same")(x)
+    encoded = tf.keras.layers.MaxPooling2D((2, 2))(x)
+
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation="relu", padding="same")(encoded)
+    x = tf.keras.layers.UpSampling2D((2, 2))(x)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation="relu", padding="same")(x)
+    x = tf.keras.layers.UpSampling2D((2, 2))(x)
+    x = tf.keras.layers.Conv2D(32, (3, 3), activation="relu", padding="same")(x)
+    x = tf.keras.layers.UpSampling2D((2, 2))(x)
+    decoded = tf.keras.layers.Conv2D(1, (3, 3), activation="relu", padding="same")(x)
+
+    autoencoder = tf.keras.Model(input_img, decoded)
+    autoencoder.compile(optimizer="adam", loss="mse")
+    autoencoder.summary()
+
+
+    autoencoder.fit(train_images, train_labels, epochs=10, batch_size=128, validation_data=(test_images, test_labels))
